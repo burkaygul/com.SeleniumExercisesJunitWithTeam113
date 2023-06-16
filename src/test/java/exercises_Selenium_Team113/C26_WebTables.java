b8e077b03a3399570419cc98f904b42f711cb728
Burkay Gül <burkaygul96@gmail.com>
2023-06-16 17:04:04 +0200

    C26

--- src/test/java/exercises_Selenium_Team113/C26_WebTables.java
+++ src/test/java/exercises_Selenium_Team113/C26_WebTables.java
@@ -8,62 +8,63 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import utilities.TestBase;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class C26_WebTables extends TestBase {
 
     @Test
     public void test01() throws InterruptedException {
+
+        // 1."https://www.amazon.com" adresine gidin
         driver.get("https://www.amazon.com");
+        // 2.Sayfanin en altina inin
         Actions actions = new Actions(driver);
-
         actions.sendKeys(Keys.END).perform();
-        Thread.sleep(3000);
+        Thread.sleep(2000);
+        // 3.Web table tum body’sini yazdirin
+        WebElement allTableBody = driver.findElement(By.tagName("tbody"));
+        System.out.println(allTableBody.getText());
+        // 4.Web table’daki satir sayisinin 9 oldugunu test edin
+        List<WebElement> allRows = driver.findElements(By.xpath("//tbody//tr"));
+         int expectedRowsNumber = 9;
+         int actualRowsNumber = allRows.size();
 
-        WebElement allBodyElement = driver.findElement(By.tagName("tbody"));
-        System.out.println(allBodyElement.getText());
+        Assert.assertEquals(expectedRowsNumber,actualRowsNumber);
 
-        Thread.sleep(1000);
-        List<WebElement> satirSayisi = driver.findElements(By.xpath("//tbody//tr"));
+        // 5.Tum satirlari yazdirin
 
-        int expectedRowsNr = 9;
-        int actualRowsNr = satirSayisi.size();
-
-        Assert.assertEquals(expectedRowsNr,actualRowsNr);
-
-        System.out.println("==========================");
-
-        for (WebElement eachRow: satirSayisi
+        int satirSayisi = 1;
+        for (WebElement eachRow: allRows
              ) {
-            System.out.println(eachRow.getText());
-            System.out.println("==========================");
+            System.out.println(satirSayisi + eachRow.getText());
+            System.out.println("==================");
+            satirSayisi++;
         }
+        // 6. Web table’daki sutun sayisinin 13 olduğunu test edin
+        List<WebElement> allColumn = driver.findElements(By.xpath("//tr[1]//td"));
 
-        List<WebElement> firstColumnData = driver.findElements(By.xpath("//tbody/tr[1]/td"));
+        int expectedColumnsNumber = 13;
+        int actualColumnsNumber = allColumn.size();
+        Assert.assertEquals(expectedColumnsNumber,actualColumnsNumber);
 
-        int expectedColumnNumber = 13;
-        int actualColumnNumber = firstColumnData.size();
-
-        Assert.assertEquals(expectedColumnNumber,actualColumnNumber);
-
-
-        List<WebElement> fifthColumn = driver.findElements(By.xpath("//tbody//tr//td[5]"));
+        // 7. 5.sutunu yazdirin
+        List<WebElement> fifthColumn = driver.findElements(By.xpath("//tr//td[5]"));
 
         for (WebElement eachFifthColumn: fifthColumn
              ) {
             System.out.println(eachFifthColumn.getText());
-            System.out.println("===============");
         }
 
-        System.out.println(hucredekiBilgiyiDondurenMethod(3, 5));
+        // 8.Satir ve sutun sayisini parametre olarak alip, hucredeki bilgiyi döndüren bir method olusturun
+
+        System.out.println(hucredekiBilgiyiDonduren(3, 1));
     }
 
-    private String hucredekiBilgiyiDondurenMethod(int satirSayisi, int sutunSayisi) {
+    private String hucredekiBilgiyiDonduren(int satirSayisi, int sutunSayisi) {
 
-        //tbody//tr[3]//td[5]
-        String dinamikXpath = "//tbody/tr["+satirSayisi+"]/td["+sutunSayisi+"]";
-        WebElement istenenDataElement = driver.findElement(By.xpath(dinamikXpath));
-        return istenenDataElement.getText();
+        //tr[7]//td[3]
+        String dinamik = "//tr["+satirSayisi+"]//td["+sutunSayisi+"]";
+        WebElement dinamikHucre = driver.findElement(By.xpath(dinamik));
+        return dinamikHucre.getText();
     }
 }
