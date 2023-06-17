3226bb820cfda9ea70bc2dd0bc14362e63bbbd18
Burkay Gül <burkaygul96@gmail.com>
2023-06-17 22:57:16 +0200

    C30

---
+++ src/test/java/exercises_Selenium_Team113/C30_ExcelOtomasyon.java
@@ -0,0 +1,45 @@
+package exercises_Selenium_Team113;
+
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class C30_ExcelOtomasyon {
+
+    @Test
+    public void test01() throws IOException {
+
+        // Excel'de ingilizce ismi Benin olan bir ulke
+        // olup olmadigini test edin
+
+        String dosyaYolu = System.getProperty("user.dir") +"/src/test/java/ulkeler.xlsx";
+
+        FileInputStream fis = new FileInputStream(dosyaYolu);
+        // System.out.println(Files.exists(Paths.get(dosyaYolu)));
+
+        Workbook workbook = WorkbookFactory.create(fis);
+
+        int lastRowNm= workbook.getSheet("Sayfa1").getLastRowNum();
+
+        boolean beninVarMi = false;
+
+        for (int i = 1; i <lastRowNm ; i++) {
+            String actualCell = workbook.getSheet("Sayfa1").getRow(i).getCell(0).toString();
+
+            if (actualCell.equalsIgnoreCase("Benin")){
+                beninVarMi = true;
+                break;
+            }
+        }
+        Assert.assertTrue(beninVarMi);
+    }
+}
